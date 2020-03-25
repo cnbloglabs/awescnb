51b8063b8c53b4743477365b785c6da820feec42
guangzan <923665892@qq.com>
2020-03-26 04:52:27 +0800

    auto deploy

--- src/plugins/imageBox.js
+++ src/plugins/imageBox.js
@@ -5,16 +5,32 @@ import '../../node_modules/baguettebox.js/dist/baguetteBox.css'
 const options = window.opts.imagebox
 // 设置图片灯箱
 function buildPostDetail() {
+    const imgList = $('#cnblogs_post_body img')
+    if (imgList === 0) return
+    $.each(imgList, i => {
+        const item = $(imgList[i])
+        // const flag = item.attr('id')
+        // if (typeof flag === 'undefined' && item.outerWidth() > 50) {
+        //     item.wrap(
+        //         "<a class='lightbox' href='" + item.attr('src') + "'></a>",
+        //     )
+        // }
+        item.wrap(`<a class='lightbox-post' href='${item.attr('src')}'></a>`)
+    })
+    baguetteBox.run('.lightbox-post')
+}
+
+function buildCommentsList() {
     const imgList = $('.feedbackCon img')
     if (imgList === 0) return
     $.each(imgList, i => {
         const item = $(imgList[i])
-        item.wrap(`<a class='lightbox' href='${item.attr('src')}'></a>`)
+        item.wrap(
+            `<a class='lightbox-comments' href='${item.attr('src')}'></a>`,
+        )
     })
-    baguetteBox.run('.lightbox')
+    baguetteBox.run('.lightbox-comments')
 }
-
-function buildCommentsList() {}
 
 // 轮询评论区
 function poll() {
