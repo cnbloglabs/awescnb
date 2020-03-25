19d18978f6e6687e486db4cf5051a83753f4d9c4
guangzan <923665892@qq.com>
2020-03-26 04:46:23 +0800

    auto deploy

--- src/plugins/imageBox.js
+++ src/plugins/imageBox.js
@@ -2,24 +2,45 @@ import { pageName } from '@/assets/utils/tools'
 import baguetteBox from '../../node_modules/baguettebox.js/dist/baguetteBox'
 import '../../node_modules/baguettebox.js/dist/baguetteBox.css'
 
+const options = window.opts.imagebox
 // 设置图片灯箱
-function imagebox() {
-    const options = window.opts.imagebox
-    if (!options.enable) return
-    if (pageName() !== 'post') return
-    
-    const imgList = $('#cnblogs_post_body img,.feedbackCon img')
+function buildPostDetail() {
+    const imgList = $('.feedbackCon img')
     if (imgList === 0) return
     $.each(imgList, i => {
         const item = $(imgList[i])
-        const flag = item.attr('id')
-        if (typeof flag === 'undefined' && item.outerWidth() > 50) {
-            item.wrap(
-                "<a class='lightbox' href='" + item.attr('src') + "'></a>",
-            )
-        }
+        item.wrap(`<a class='lightbox' href='${item.attr('src')}'></a>`)
     })
     baguetteBox.run('.lightbox')
 }
 
+function buildCommentsList() {}
+
+// 轮询评论区
+function poll() {
+    if (pageName() !== 'post') return
+    if ($('.blog_comment_body').length) {
+        buildCommentsList()
+    } else {
+        let count = 1
+
+        let intervalId = setInterval(() => {
+            if ($('.blog_comment_body').length) {
+                clearInterval(intervalId)
+                buildCommentsList()
+            }
+            if (count === 15) {
+                clearInterval(intervalId)
+            }
+            count++
+        }, 500)
+    }
+}
+
+function imagebox() {
+    if (!options.enable) return
+    if (pageName() !== 'post') return
+    buildPostDetail()
+    poll()
+}
 export default imagebox
