1f4c798b55924c171d24cb94dd9bb411346ca6b8
guangzan <923665892@qq.com>
2020-03-31 00:56:48 +0800

    🐞fix(reacg>constants>message):

--- src/themes/reacg/constants/message.js
+++ src/themes/reacg/constants/message.js
@@ -1,8 +1,8 @@
  const message = {
      version: 'v2.0',
      author: 'DIV Monster',
-     blog: 'https://www.cnblogs.com/guangzan/',
-     gitee: 'https://gitee.com/guangzan/awescnb/',
+     blog: 'https://www.cnblogs.com/guangzan',
+     gitee: 'https://gitee.com/guangzan/awescnb',
  }
 
  export default message
