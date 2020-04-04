947759710be76193fa851232ce7f7cc81585af35
guangzan <923665892@qq.com>
2020-04-05 00:16:01 +0800

    🌈style(post top image): use random image

--- src/plugins/postTopimage/index.js
+++ src/plugins/postTopimage/index.js
@@ -1,24 +1,18 @@
 // 在随笔详情页顶部随笔生成一个文章头图
 // 引入即可
-import { randomImgurl, pageName, randomArrayElements } from '@tools'
+import { pageName, randomArrayElements } from '@tools'
+import { randomImage } from '@constants/urls'
 import './index.css'
 
-const opts = window.opts.postTopimage
+const { enable, urls } = window.opts.postTopimage
 
 function postTopimage() {
-    const enable = opts.enable
-    const urls = opts.urls
     if (!enable) return
     if (pageName() !== 'post') return
-    let url = ''
-    if (urls.length === 0) {
-        url = randomImgurl()
-    } else {
-        url = randomArrayElements(urls, 1)[0]
-    }
+    let url = urls.length === 0 ? randomImage : randomArrayElements(urls, 1)[0]
     const ele = `<div id="custom-post-topimage"></div>`
     $('.post>.postTitle').after(ele)
-    $('#custom-post-topimage').css('background-image',`url(${url})`)
+    $('#custom-post-topimage').css('background-image', `url(${url})`)
 }
 
 export default postTopimage
