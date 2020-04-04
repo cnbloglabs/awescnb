8c21524845f8863b7620f009560a505771ed21d7
guangzan <923665892@qq.com>
2020-04-05 00:14:33 +0800

    ✨feat(urls): random image

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,8 +1,9 @@
 const themeRepository = 'https://guangzan.gitee.io/awescnb'
 const animeImages = 'https://guangzan.gitee.io/imagehost/awescnb/images/anime'
+const randomImage = 'https://api.mz-moe.cn/img.php'
 const js = 'https://guangzan.gitee.io/imagehost/awescnb/js'
 const live2d = {
     url: 'https://cdn.jsdelivr.net/gh/github923665892/awesCnb-live2dModels',
     version: '1.7',
 }
-export { live2d, themeRepository, animeImages, js }
+export { live2d, themeRepository, animeImages, js, randomImage }
