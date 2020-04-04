2030a154ad62688ce9405a6747557336cfb1935c
guangzan <923665892@qq.com>
2020-04-05 00:51:38 +0800

    ✨feat(api): anime image api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
 const bing = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+const animeImage = 'https://acg.toubiec.cn/random.php?return=img'
 
-export { bing }
+export { bing, animeImage }
