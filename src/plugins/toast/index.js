d4a7c3693309e61a271ea03b6dda89e339479faf
guangzan <923665892@qq.com>
2020-04-07 19:30:07 +0800

    ✨feat(plugins): add toast

---
+++ src/plugins/toast/index.js
@@ -0,0 +1,16 @@
+// 弹出提示
+import './index.css'
+import { materialize } from '@constants/urls'
+import { cacheScript } from '@tools'
+
+function build(title, duration) {
+    window.Materialize.toast(title, duration)
+}
+
+function toast(title = '默认提示文字', duration = 5000) {
+    cacheScript(materialize, () => {
+        build(title, duration)
+    })
+}
+
+export default toast
