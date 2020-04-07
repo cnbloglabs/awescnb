48b3bc78a214d6cded6f9d12f1b09bad2220a03b
guangzan <923665892@qq.com>
2020-04-08 04:19:00 +0800

    🌈style(plugins about code): style

--- src/plugins/linenumbers/index.js
+++ src/plugins/linenumbers/index.js
@@ -1,12 +1,13 @@
 // 设置代码行号
 // 引入即可
-
+import './index.css'
 import { pageName } from '@/assets/utils/tools'
 
 function linenumbers() {
     const options = window.opts.lineNumbers
     if (pageName() !== 'post') return
     if (!options.enable) return
+    if ($('.cnblogs_code').length > 0) return
     require('@/assets/js/linenumbers')
     hljs.initLineNumbersOnLoad()
 }
