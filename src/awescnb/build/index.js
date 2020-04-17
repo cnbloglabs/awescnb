23586cbe35529ffd1ceb9af267d03e44f1971232
guangzan <923665892@qq.com>
2020-04-17 22:07:16 +0800

    🦄refactor(awescnb>build): del notice

--- src/awescnb/build/index.js
+++ src/awescnb/build/index.js
@@ -1,27 +1,15 @@
 import printing from './printing'
 import grayDay from './grayDay'
-import { shootCustom } from '@plugins/barrage'
 
-const noticeContent = []
-
-// 隐藏加载动画
 function hideLoading() {
     setTimeout(() => {
         $('#loading').fadeOut(200)
     }, 180)
 }
 
-// notice
-function notice() {
-    if (noticeContent.length === 0) return
-    shootCustom(noticeContent)
-}
-
-// build
 function build() {
     hideLoading()
     printing()
-    notice()
     grayDay()
 }
 
