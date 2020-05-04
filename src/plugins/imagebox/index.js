e37c8f8b160316405f8a59f8b925f31be41d80a3
guangzan <923665892@qq.com>
2020-05-04 12:06:18 +0800

    🐞fix(plugins>imgbox):

--- src/plugins/imagebox/index.js
+++ src/plugins/imagebox/index.js
@@ -3,59 +3,27 @@
 
 import './index.css'
 import { pageName, poll } from '@/assets/utils/tools'
-// import baguetteBox from '../../../node_modules/baguettebox.js/dist/baguetteBox'
-// import '../../../node_modules/baguettebox.js/dist/baguetteBox.css'
 import mediumZoom from 'medium-zoom'
 
 const options = window.opts.imagebox
 const mediumZoomConfig = {
-    scrollOffset: 0,
     background: 'rgba(0,0,0,0.3)',
 }
 
-function buildPostDetail() {
-    const imgList = $('#cnblogs_post_body img')
+function build() {
+    const imgList = $('.forFlow img')
     if (imgList === 0) return
     $.each(imgList, i => {
         const item = $(imgList[i])
-        // const flag = item.attr('id')
-        // if (typeof flag === 'undefined' && item.outerWidth() > 50) {
-        //     item.wrap(
-        //         "<a class='lightbox' href='" + item.attr('src') + "'></a>",
-        //     )
-        // }
-        // item.wrap(`<a class='custom-zoom' href='${item.attr('src')}'></a>`)
         item.addClass('custom-zoom')
     })
 
-    // baguetteBox.run('.custom-zoom')
     mediumZoom('.custom-zoom', mediumZoomConfig)
-}
-
-function buildCommentsList() {
-    const imgList = $('.feedbackCon img')
-    if (imgList === 0) return
-    $.each(imgList, i => {
-        const item = $(imgList[i])
-        // item.wrap(`<a class='custom-zoom' href='${item.attr('src')}'></a>`)
-        item.addClass('custom-zoom')
-    })
-    // baguetteBox.run('.custom-zoom', {
-    //     animation: 'fadeIn',
-    // })
-    mediumZoom('.custom-zoom', mediumZoomConfig)
-}
-
-// 轮询评论区
-function pollToShow() {
-    if (pageName() !== 'post') return
-    poll($('.blog_comment_body').length, buildCommentsList)
 }
 
 function imagebox() {
     if (!options.enable) return
     if (pageName() !== 'post') return
-    buildPostDetail()
-    pollToShow()
+    poll($('.blog_comment_body').length, build)
 }
 export default imagebox
