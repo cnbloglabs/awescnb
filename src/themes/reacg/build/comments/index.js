3ceb03020cf3d50e4824612b6df4b037e133268e
guangzan <923665892@qq.com>
2020-05-08 01:36:39 +0800

    🐞fix(sub event):

--- src/themes/reacg/build/comments/index.js
+++ src/themes/reacg/build/comments/index.js
@@ -2,17 +2,28 @@
 import toast from '@plugins/toast'
 import './index.scss'
 
-// const commentManager = window.commentManager
+const commentManager = window.commentManager
+const awesCommentsAvatar = window.awesCommentsAvatar
 
 function submit() {
     $('#btn_comment_submit').click(() => {
         toast('感谢评论 🍺')
-        window.RefreshCommentList()
+        commentManager.renderComments(0)
+        awesCommentsAvatar()
+    })
+}
+
+function del() {
+    $('.comment_actions a:nth-child(2)').click(() => {
+        toast('删除成功 ✔')
+        commentManager.renderComments(0)
+        awesCommentsAvatar()
     })
 }
 
 function comments() {
     submit()
+    del()
 }
 
 export default comments
