4ce6571da3e1fbd3fb7694a379262eb5944177bb
guangzan <923665892@qq.com>
2020-05-15 06:33:09 +0800

    🐞fix(ele>toast): only post

--- src/themes/element/plugins/toast/index.js
+++ src/themes/element/plugins/toast/index.js
@@ -1,10 +1,15 @@
 // 消息弹窗
 import './index.css'
-import { cacheScript } from '@tools'
+import { cacheScript, pageName } from '@tools'
 import { notiflix } from '../../constants/urls'
 
-function build() {
+function postPageNotice() {
+    if (pageName() !== 'post') return
     window.Notiflix.Notify.Success('已生成博文目录，点击右下角图标查看')
+}
+
+function build() {
+    postPageNotice()
 }
 
 export default function toast() {
