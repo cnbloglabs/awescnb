98dc0a5285347c54eebea078a33d79d51ebe2a2c
guangzan <923665892@qq.com>
2020-05-16 01:05:58 +0800

    🦄refactor(reacg>build): scroll

---
+++ src/themes/reacg/build/scroll/index.js
@@ -0,0 +1,43 @@
+import './index.scss'
+
+// 只触发一次向上或向下
+// 如果又重新反向滚动则再触发一次
+function scrollOnce() {
+    function scrollFunc() {
+        let scrollDirection
+        if (!scrollAction) {
+            scrollAction = window.pageYOffset
+        }
+        let diff = scrollAction - window.pageYOffset
+        if (diff < 0) {
+            scrollDirection = 'down'
+        } else if (diff > 0) {
+            scrollDirection = 'up'
+        } else {
+            // First scroll event
+        }
+        scrollAction = window.pageYOffset
+        return scrollDirection
+    }
+    let scrollAction, originalDir
+
+    $(window).scroll(function() {
+        let direction = scrollFunc()
+        if (direction && originalDir != direction) {
+            if (direction == 'down') {
+                $('#header').addClass('header-hide') // 头部导航隐藏
+                $('#catalog').addClass('catalog-scroll-up') // 当头部导航隐藏目录随之向上移动
+            } else {
+                $('#header').removeClass('header-hide')
+                $('#catalog').removeClass('catalog-scroll-up')
+            }
+            originalDir = direction
+        }
+    })
+}
+
+function scroll() {
+    scrollOnce()
+}
+
+export default scroll
