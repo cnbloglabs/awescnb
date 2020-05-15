471d2fdca42cf95caa71f5a4616efe801370b075
guangzan <923665892@qq.com>
2020-05-16 01:40:10 +0800

    ✨feat(plugins>highlight): add pande

--- src/plugins/highlight/index.js
+++ src/plugins/highlight/index.js
@@ -2,35 +2,34 @@
 import { pageName } from '@/assets/utils/tools'
 import './index.css'
 
-const opts = window.opts.highLight
+const {type} = window.opts.highLight
 
-function build() {
-    const atomOneDark = ` --hl-base: #282c34;
-                            --hl-mono-1: #abb2bf;
-                            --hl-mono-3: #5c6370;
-                            --hl-hue-1: #56b6c2;
-                            --hl-hue-2: #61aeee;
-                            --hl-hue-3: #c678dd;
-                            --hl-hue-4: #98c379;
-                            --hl-hue-5: #e06c75;
-                            --hl-hue-6: #d19a66;
-                            --hl-hue-6-2: #e6c07b;
-                            --hl-tag: #abb2bf;`
+const atomOneDark = `--hl-base: #282c34;
+                         --hl-mono-1: #abb2bf;
+                         --hl-mono-3: #5c6370;
+                         --hl-hue-1: #56b6c2;
+                         --hl-hue-2: #61aeee;
+                         --hl-hue-3: #c678dd;
+                         --hl-hue-4: #98c379;
+                         --hl-hue-5: #e06c75;
+                         --hl-hue-6: #d19a66;
+                         --hl-hue-6-2: #e6c07b;
+                         --hl-tag: #abb2bf;`
 
-    // atomOneLight --hl-base: #fafafa;
-    const atomOneLight = `--hl-base: #f6f8fa;
-                            --hl-mono-1: #383a42;
-                            --hl-mono-3: #a0a1a7;
-                            --hl-hue-1: #0184bb;
-                            --hl-hue-2: #4078f2;
-                            --hl-hue-3: #a626a4;
-                            --hl-hue-4: #50a14f;
-                            --hl-hue-5: #e45649;
-                            --hl-hue-6: #986801;
-                            --hl-hue-6-2: #c18401;
-                            --hl-tag: #383A42;`
+// atomOneLight --hl-base: #fafafa;
+const atomOneLight = `--hl-base: #f6f8fa;
+                          --hl-mono-1: #383a42;
+                          --hl-mono-3: #a0a1a7;
+                          --hl-hue-1: #0184bb;
+                          --hl-hue-2: #4078f2;
+                          --hl-hue-3: #a626a4;
+                          --hl-hue-4: #50a14f;
+                          --hl-hue-5: #e45649;
+                          --hl-hue-6: #986801;
+                          --hl-hue-6-2: #c18401;
+                          --hl-tag: #383A42;`
 
-    const github = `--hl-base: #f6f8fa;
+const github = `--hl-base: #f6f8fa;
                     --hl-mono-1: #333333;
                     --hl-mono-3: #d73a49;
                     --hl-hue-1: #0086b3;
@@ -42,13 +41,27 @@ function build() {
                     --hl-hue-6-2: #6f42c1;
                     --hl-tag: #d73a49;`
 
+const pande = `   --hl-base: #292a2b;
+  --hl-mono-1: #e6e6e6;
+  --hl-mono-3: #ffcc95;
+  --hl-hue-1: #0086b3;
+  --hl-hue-2: #0086b3;
+  --hl-hue-3: #d73a49;
+  --hl-hue-4: #19f9d866;
+  --hl-hue-5: #19f9d8;
+  --hl-hue-6: #ff4b82;
+  --hl-hue-6-2: #ff4b82;
+  --hl-tag: #ffb86c;`
+
+function build() {
     const actions = {
         atomOneDark,
         atomOneLight,
         github,
+        pande,
     }
 
-    const style = `<style>:root{${actions[opts.type]}}</style>`
+    const style = `<style>:root{${actions[type]}}</style>`
     $('head').append(style)
 }
 
