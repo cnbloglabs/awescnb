34b9f1120caeb538c4139a59e1f9cba46c941a86
guangzan <923665892@qq.com>
2020-05-21 00:42:42 +0800

    🎈perf(reacg>ava): remove dev url

--- src/themes/reacg/build/avatar/index.js
+++ src/themes/reacg/build/avatar/index.js
@@ -1,12 +1,8 @@
 // 设置侧边栏头像
 import './index.scss'
-import env from '@constants/env'
 
 function avatar() {
-    let avatar = window.opts.theme.avatar
-    if (env === 'dev') {
-        avatar = 'https://dummyimage.com/50'
-    }
+    const avatar = window.opts.theme.avatar
     if (avatar === '' || $('#blog-news').length === 0) return
     $('#blog-news').prepend(`<img id='custom-avatar' src='${avatar}' />`)
 }
