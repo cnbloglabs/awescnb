32a8fd21aefbbf3f0cdae5ea808e57772d35303b
guangzan <923665892@qq.com>
2020-09-23 08:34:46 +0800

    auto deploy

---
+++ src/plugins/index.js
@@ -0,0 +1,69 @@
+import back2top from '@plugins/back2top'
+import background from '@plugins/background'
+import { shootInitial } from '@plugins/barrage'
+import btns from '@plugins/btns'
+import catalog from '@plugins/catalog'
+import charts from '@plugins/charts'
+import click from '@plugins/click'
+import commentsAvatars from '@plugins/commentsAvatars'
+import copy from '@plugins/copy'
+import donation from '@plugins/donation'
+import dragMenu from '@plugins/dragMenu'
+import emoji from '@plugins/emoji'
+import footer from '@plugins/footer'
+import highlight from '@plugins/highlight'
+import imagebox from '@plugins/imagebox'
+import linenumbers from '@plugins/linenumbers'
+import live2d from '@plugins/live2d'
+import lock from '@plugins/lock'
+import mode from '@plugins/mode'
+import notation from '@plugins/notation'
+import notice from '@plugins/notice'
+import particle from '@plugins/particle'
+import player from '@plugins/player'
+import postBottomimage from '@plugins/postBottomimage'
+import postMessage from '@plugins/postMessage'
+import postSignature from '@plugins/postSignature'
+import postTopimage from '@plugins/postTopimage'
+import progress from '@plugins/progress'
+import qrcode from '@plugins/qrcode'
+import signature from '@plugins/signature'
+import themeColor from '@plugins/themeColor'
+import titleFavicon from '@plugins/titleFavicon'
+import toast from '@plugins/toast'
+
+export {
+    back2top,
+    background,
+    shootInitial,
+    btns,
+    catalog,
+    charts,
+    click,
+    commentsAvatars,
+    copy,
+    donation,
+    dragMenu,
+    emoji,
+    footer,
+    highlight,
+    imagebox,
+    linenumbers,
+    live2d,
+    lock,
+    mode,
+    notation,
+    notice,
+    particle,
+    player,
+    postBottomimage,
+    postMessage,
+    postSignature,
+    postTopimage,
+    progress,
+    qrcode,
+    signature,
+    themeColor,
+    titleFavicon,
+    toast,
+}
