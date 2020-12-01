e20ce4cbfa0f0b715a3d298681172322f8e37b6e
guangzan <923665892@qq.com>
2020-12-01 13:29:46 +0800

    auto deploy

--- src/awescnb/utils.js
+++ src/awescnb/utils.js
@@ -1,10 +1,14 @@
 function getUserinfo() {
     console.log('fn run')
-    $(document).ajaxComplete(function(event, xhr, option) {
-        console.log('any ajax complete', event, xhr, option)
-        if (option.url.indexOf('userinfo') > -1) {
-            console.log('get userinfo', event, xhr, option)
-        }
+    // $(document).ajaxComplete(function(event, xhr, option) {
+    //     console.log('any ajax complete', event, xhr, option)
+    //     if (option.url.indexOf('userinfo') > -1) {
+    //         console.log('get userinfo', event, xhr, option)
+    //     }
+    // })
+
+    $(document).ajaxSuccess((event, xhr, option) => {
+        console.log(event, xhr, option)
     })
 }
 
