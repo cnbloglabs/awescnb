634c4bf9818e96bc4d1104cac7bdce5cc3440393
guangzan <923665892@qq.com>
2021-03-04 17:28:05 +0800

    auto deploy

--- src/plugins/click/index.js
+++ src/plugins/click/index.js
@@ -1,210 +1,154 @@
-import { animeJs } from '@constants/urls'
-import { clickConfig } from '@config/plugins.js'
-import { userAgent, cacheScript } from '@tools'
-
 /**
- * 构建点击特效
- * @param {*} options
+ * @Author ColdDay
+ * @Github https://github.com/ColdDay/click-colorful/blob/master/click-colorful.js
  */
-function build(options) {
-    const anime = window.anime
-    if (options.enable && userAgent() === 'pc') {
-        $('body').append(
-            `<canvas id="click-effects"></canvas>`,
-        )
-        window.human = false
-        let canvasEl = document.querySelector(
-            '#click-effects',
-        )
-        let ctx = canvasEl.getContext('2d')
-        let numberOfParticules = options.maxCount
-        let pointerX = 0
-        let pointerY = 0
+import { clickConfig } from '@config/plugins.js'
 
-        let tap =
-            'ontouchstart' in window ||
-            navigator.msMaxTouchPoints
-                ? 'touchstart'
-                : 'mousedown'
+const defaultParams = {
+    colors: ['#eb125f', '#6eff8a', '#6386ff', '#f9f383'],
+    size: 30,
+    maxCount: 30,
+}
 
-        let colors = options.colors.length
-            ? options.colors
-            : ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C']
-
-        const clickEffects = () => {
-            canvasEl.style.position = 'fixed'
-            canvasEl.style.top = 0
-            canvasEl.width = window.innerWidth * 2
-            canvasEl.height = window.innerHeight * 2
-            canvasEl.style.width = window.innerWidth + 'px'
-            canvasEl.style.height =
-                window.innerHeight + 'px'
-            canvasEl.getContext('2d').scale(2, 2)
-        }
-
-        const updateCoords = e => {
-            pointerX = e.clientX || e.touches[0].clientX
-            pointerY = e.clientY || e.touches[0].clientY
-        }
-
-        const setParticuleDirection = p => {
-            let angle =
-                (anime.random(0, 360) * Math.PI) / 180
-            let value = anime.random(50, 180)
-            let radius = [-1, 1][anime.random(0, 1)] * value
-            return {
-                x: p.x + radius * Math.cos(angle),
-                y: p.y + radius * Math.sin(angle),
-            }
-        }
-
-        const createParticule = (x, y) => {
-            let p = {}
-            p.x = x
-            p.y = y
-            p.color =
-                colors[anime.random(0, colors.length - 1)]
-            p.radius = anime.random(16, 32)
-            p.endPos = setParticuleDirection(p)
-            p.draw = function() {
-                ctx.beginPath()
-                ctx.arc(
-                    p.x,
-                    p.y,
-                    p.radius,
-                    0,
-                    2 * Math.PI,
-                    true,
-                )
-                ctx.fillStyle = p.color
-                ctx.fill()
-            }
-            return p
-        }
-
-        const createCircle = (x, y) => {
-            let p = {}
-            p.x = x
-            p.y = y
-            p.color = '#FFF'
-            p.radius = 0.1
-            p.alpha = 0.5
-            p.lineWidth = 6
-            p.draw = function() {
-                ctx.globalAlpha = p.alpha
-                ctx.beginPath()
-                ctx.arc(
-                    p.x,
-                    p.y,
-                    p.radius,
-                    0,
-                    2 * Math.PI,
-                    true,
-                )
-                ctx.lineWidth = p.lineWidth
-                ctx.strokeStyle = p.color
-                ctx.stroke()
-                ctx.globalAlpha = 1
-            }
-            return p
-        }
-
-        const renderParticule = anim => {
-            for (
-                let i = 0;
-                i < anim.animatables.length;
-                i++
-            ) {
-                anim.animatables[i].target.draw()
-            }
-        }
-
-        const animateParticules = (x, y) => {
-            let circle = createCircle(x, y)
-            let particules = []
-            for (let i = 0; i < numberOfParticules; i++) {
-                particules.push(createParticule(x, y))
-            }
-            anime
-                .timeline()
-                .add({
-                    targets: particules,
-                    x(p) {
-                        return p.endPos.x
-                    },
-                    y(p) {
-                        return p.endPos.y
-                    },
-                    radius: 0.1,
-                    duration: anime.random(1200, 1800),
-                    easing: 'easeOutExpo',
-                    update: renderParticule,
-                })
-                .add({
-                    targets: circle,
-                    radius: anime.random(80, 160),
-                    lineWidth: 0,
-                    alpha: {
-                        value: 0,
-                        easing: 'linear',
-                        duration: anime.random(600, 800),
-                    },
-                    duration: anime.random(1200, 1800),
-                    easing: 'easeOutExpo',
-                    update: renderParticule,
-                    offset: 0,
-                })
-        }
-
-        const render = anime({
-            duration: Infinity,
-            update() {
-                ctx.clearRect(
-                    0,
-                    0,
-                    canvasEl.width,
-                    canvasEl.height,
-                )
-            },
-        })
-
-        document.addEventListener(
-            tap,
-            function(e) {
-                window.human = true
-                render.play()
-                updateCoords(e)
-                animateParticules(pointerX, pointerY)
-            },
-            false,
-        )
-
-        const autoClick = () => {
-            let centerX = window.innerWidth / 2
-            let centerY = window.innerHeight / 2
-            if (window.human) return
-            animateParticules(
-                anime.random(centerX - 50, centerX + 50),
-                anime.random(centerY - 50, centerY + 50),
-            )
-            anime({
-                duration: 200,
-            }).finished.then(autoClick)
-        }
-
-        if (options.auto) autoClick()
-        window.addEventListener(
-            'resize',
-            clickEffects,
-            false,
-        )
-        clickEffects()
+function colorBall(params) {
+    this.params = Object.assign({}, defaultParams, params)
+    if (!params.colors.length) {
+        this.params.colors = defaultParams.colors
     }
+}
+
+function getOneRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function _run(ball) {
+    let randomXFlag = Math.random() > 0.5
+    let randomYFlag = Math.random() > 0.5
+    let randomX = parseInt(Math.random() * 160)
+    let randomY = parseInt(Math.random() * 160)
+    if (randomXFlag) {
+        randomX = randomX * -1
+    }
+    if (randomYFlag) {
+        randomY = randomY * -1
+    }
+    let transform =
+        'translate3d(' +
+        randomX +
+        'px,' +
+        randomY +
+        'px, 0) scale(0)'
+    ball.style.webkitTransform = transform
+    ball.style.MozTransform = transform
+    ball.style.msTransform = transform
+    ball.style.OTransform = transform
+    ball.style.transform = transform
+}
+
+colorBall.prototype.fly = function(
+    x,
+    y,
+    playCount,
+    loopTimer,
+) {
+    if (!loopTimer) loopTimer = 300
+
+    let ballElements = []
+    let fragment = document.createDocumentFragment()
+    let ballNum = this.params.maxCount
+
+    if (playCount) {
+        ballNum = ballNum * playCount
+    }
+
+    let loop = 0
+
+    for (let i = 0; i < ballNum; i++) {
+        let curLoop = parseInt(i / this.params.maxCount)
+        let ball = document.createElement('i')
+        ball.className = 'color-ball ball-loop-' + curLoop
+        let blurX = Math.random() * 10
+        if (Math.random() > 0.5) blurX = blurX * -1
+        let blurY = Math.random() * 10
+        if (Math.random() > 0.5) blurY = blurY * -1
+        ball.style.left = x + 'px'
+        ball.style.top = y + 'px'
+        ball.style.width = this.params.size + 'px'
+        ball.style.height = this.params.size + 'px'
+        ball.style.position = 'fixed'
+        ball.style.borderRadius = '1000px'
+        ball.style.boxSizing = 'border-box'
+        ball.style.zIndex = 9999
+        ball.style.opacity = 0
+        if (curLoop === 0) ball.style.opacity = 1
+        ball.style.transform =
+            'translate3d(0px, 0px, 0px) scale(1)'
+        ball.style.transition =
+            'transform 0.7s ' +
+            (curLoop * loopTimer) / 1000 +
+            's ease-out'
+        ball.style.backgroundColor = getOneRandom(
+            this.params.colors,
+        )
+        fragment.appendChild(ball)
+        ballElements.push(ball)
+
+        if (curLoop !== loop) {
+            // eslint-disable-next-line no-extra-semi
+            ;(function(num) {
+                setTimeout(function() {
+                    let loopBalls = document.getElementsByClassName(
+                        'ball-loop-' + num,
+                    )
+                    for (
+                        let j = 0;
+                        j < loopBalls.length;
+                        j++
+                    ) {
+                        loopBalls[j].style.opacity = 1
+                    }
+                    if (num === loop) {
+                        _clear(ballElements)
+                    }
+                }, num * loopTimer + 30)
+            })(curLoop)
+            loop = curLoop
+        }
+    }
+
+    document.body.appendChild(fragment)
+    // 延迟删除
+    !playCount && _clear(ballElements)
+    // 执行动画
+    setTimeout(function() {
+        for (let i = 0; i < ballElements.length; i++) {
+            _run(ballElements[i])
+        }
+    }, 10)
+}
+
+function _clear(balls) {
+    setTimeout(function() {
+        for (let i = 0; i < balls.length; i++) {
+            document.body.removeChild(balls[i])
+        }
+    }, 1000)
+}
+
+const build = options => {
+    const colorBallConfig = {
+        colors: options.colors,
+        size: options.size,
+        maxCount: options.maxCount,
+    }
+    const color = new colorBall(colorBallConfig)
+
+    $('body').click(e => {
+        color.fly(e.clientX, e.clientY)
+    })
 }
 
 export default devOptions => {
     const options = clickConfig(devOptions)
-    cacheScript(animeJs, () => {
-        build(options)
-    })
+    options.enable && build(options)
 }
---
+++ src/plugins/clickbak/index.js
@@ -0,0 +1,213 @@
+/**
+ * Disabled!
+ */
+import { animeJs } from '@constants/urls'
+import { clickConfig } from '@config/plugins.js'
+import { userAgent, cacheScript } from '@tools'
+
+/**
+ * 构建点击特效
+ * @param {*} options
+ */
+function build(options) {
+    const anime = window.anime
+    if (options.enable && userAgent() === 'pc') {
+        $('body').append(
+            `<canvas id="click-effects"></canvas>`,
+        )
+        window.human = false
+        let canvasEl = document.querySelector(
+            '#click-effects',
+        )
+        let ctx = canvasEl.getContext('2d')
+        let numberOfParticules = options.maxCount
+        let pointerX = 0
+        let pointerY = 0
+
+        let tap =
+            'ontouchstart' in window ||
+            navigator.msMaxTouchPoints
+                ? 'touchstart'
+                : 'mousedown'
+
+        let colors = options.colors.length
+            ? options.colors
+            : ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C']
+
+        const clickEffects = () => {
+            canvasEl.style.position = 'fixed'
+            canvasEl.style.top = 0
+            canvasEl.width = window.innerWidth * 2
+            canvasEl.height = window.innerHeight * 2
+            canvasEl.style.width = window.innerWidth + 'px'
+            canvasEl.style.height =
+                window.innerHeight + 'px'
+            canvasEl.getContext('2d').scale(2, 2)
+        }
+
+        const updateCoords = e => {
+            pointerX = e.clientX || e.touches[0].clientX
+            pointerY = e.clientY || e.touches[0].clientY
+        }
+
+        const setParticuleDirection = p => {
+            let angle =
+                (anime.random(0, 360) * Math.PI) / 180
+            let value = anime.random(50, 180)
+            let radius = [-1, 1][anime.random(0, 1)] * value
+            return {
+                x: p.x + radius * Math.cos(angle),
+                y: p.y + radius * Math.sin(angle),
+            }
+        }
+
+        const createParticule = (x, y) => {
+            let p = {}
+            p.x = x
+            p.y = y
+            p.color =
+                colors[anime.random(0, colors.length - 1)]
+            p.radius = anime.random(16, 32)
+            p.endPos = setParticuleDirection(p)
+            p.draw = function() {
+                ctx.beginPath()
+                ctx.arc(
+                    p.x,
+                    p.y,
+                    p.radius,
+                    0,
+                    2 * Math.PI,
+                    true,
+                )
+                ctx.fillStyle = p.color
+                ctx.fill()
+            }
+            return p
+        }
+
+        const createCircle = (x, y) => {
+            let p = {}
+            p.x = x
+            p.y = y
+            p.color = '#FFF'
+            p.radius = 0.1
+            p.alpha = 0.5
+            p.lineWidth = 6
+            p.draw = function() {
+                ctx.globalAlpha = p.alpha
+                ctx.beginPath()
+                ctx.arc(
+                    p.x,
+                    p.y,
+                    p.radius,
+                    0,
+                    2 * Math.PI,
+                    true,
+                )
+                ctx.lineWidth = p.lineWidth
+                ctx.strokeStyle = p.color
+                ctx.stroke()
+                ctx.globalAlpha = 1
+            }
+            return p
+        }
+
+        const renderParticule = anim => {
+            for (
+                let i = 0;
+                i < anim.animatables.length;
+                i++
+            ) {
+                anim.animatables[i].target.draw()
+            }
+        }
+
+        const animateParticules = (x, y) => {
+            let circle = createCircle(x, y)
+            let particules = []
+            for (let i = 0; i < numberOfParticules; i++) {
+                particules.push(createParticule(x, y))
+            }
+            anime
+                .timeline()
+                .add({
+                    targets: particules,
+                    x(p) {
+                        return p.endPos.x
+                    },
+                    y(p) {
+                        return p.endPos.y
+                    },
+                    radius: 0.1,
+                    duration: anime.random(1200, 1800),
+                    easing: 'easeOutExpo',
+                    update: renderParticule,
+                })
+                .add({
+                    targets: circle,
+                    radius: anime.random(80, 160),
+                    lineWidth: 0,
+                    alpha: {
+                        value: 0,
+                        easing: 'linear',
+                        duration: anime.random(600, 800),
+                    },
+                    duration: anime.random(1200, 1800),
+                    easing: 'easeOutExpo',
+                    update: renderParticule,
+                    offset: 0,
+                })
+        }
+
+        const render = anime({
+            duration: Infinity,
+            update() {
+                ctx.clearRect(
+                    0,
+                    0,
+                    canvasEl.width,
+                    canvasEl.height,
+                )
+            },
+        })
+
+        document.addEventListener(
+            tap,
+            function(e) {
+                window.human = true
+                render.play()
+                updateCoords(e)
+                animateParticules(pointerX, pointerY)
+            },
+            false,
+        )
+
+        const autoClick = () => {
+            let centerX = window.innerWidth / 2
+            let centerY = window.innerHeight / 2
+            if (window.human) return
+            animateParticules(
+                anime.random(centerX - 50, centerX + 50),
+                anime.random(centerY - 50, centerY + 50),
+            )
+            anime({
+                duration: 200,
+            }).finished.then(autoClick)
+        }
+
+        if (options.auto) autoClick()
+        window.addEventListener(
+            'resize',
+            clickEffects,
+            false,
+        )
+        clickEffects()
+    }
+}
+
+export default devOptions => {
+    const options = clickConfig(devOptions)
+    cacheScript(animeJs, () => {
+        build(options)
+    })
+}
