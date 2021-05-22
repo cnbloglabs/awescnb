3f2cf81f9c9573bf1ba49154b75896b2e22ca32a
guangzan <923665892@qq.com>
2021-05-22 17:04:10 +0800

    refactor(theme view): 将 plugins 移至 index

--- src/themes/view/index.js
+++ src/themes/view/index.js
@@ -1,9 +1,67 @@
 import './style/index.scss'
 import { createTheme } from 'awescnb'
-import plugins from './plugins'
 import build from './build'
+import {
+    footer,
+    copyCode,
+    linenumbers,
+    postSignature,
+    commentsAvatars,
+    toolMenu,
+    mode,
+    highlight,
+    catalog,
+    themeColor,
+} from 'plugins/index'
 
 export const theme = createTheme()
 
 build()
-plugins()
+
+theme
+    .use(footer)
+    .use(copyCode)
+    .use(linenumbers)
+    .use(postSignature)
+    .use(commentsAvatars)
+    .use(highlight)
+    .use(
+        toolMenu,
+        { initialOpen: false },
+        {
+            toolbarItems: [
+                {
+                    icon: 'fa-comment-dots',
+                },
+                {
+                    icon: 'fa-star',
+                },
+                {
+                    icon: 'fa-heart',
+                },
+                {
+                    icon: 'fa-thumbs-up',
+                },
+                {
+                    enable: true,
+                    icon: 'fa-adjust',
+                },
+                {
+                    icon: 'fa-rocket',
+                },
+            ],
+        },
+    )
+    .use(mode)
+    .use(themeColor, { color: '#3273DC' })
+    .use(
+        catalog,
+        {},
+        {
+            selector: '#mainContent',
+            fn: 'append',
+            updateNavigation: true,
+            showTitle: false,
+            showScrollbar: false,
+        },
+    )
