7181b2eff5e8ac498149faa451ae07c8670febda
guangzan <923665892@qq.com>
2021-06-02 17:40:47 +0800

    auto deploy

--- src/themes/geek/build/profile/index.js
+++ src/themes/geek/build/profile/index.js
@@ -20,69 +20,158 @@ import {
     unfollow,
 } from 'utils/cnblog'
 
-function createElements() {
+function createContainer() {
+    return $('<div>').addClass('profile')
+}
+
+function createSignature() {
+    return $('<div>').addClass('profile-signature')
+}
+
+function createBackground() {
+    const { headerBackground } = getThemeOptions()
+    const signatureWrap = createSignature()
+    return $('<div>')
+        .addClass('profile-banner')
+        .css('backgroundImage', `url(${headerBackground})`)
+        .append(signatureWrap)
+}
+
+function createMenu() {
+    const el = $('<div>').addClass('profile-menu')
+    const menuItem = [
+        {
+            title: '收藏',
+            url: appWz,
+        },
+        {
+            title: '闪存',
+            url: appIng,
+        },
+        {
+            title: '小组',
+            url: appGroup,
+        },
+        {
+            title: '博问',
+            url: appQ,
+        },
+    ]
+
+    menuItem.forEach(item => {
+        const menuItem = $('<a>')
+            .attr('target', '_blank')
+            .attr('href', item.url)
+            .text(item.title)
+
+        el.append(menuItem)
+    })
+
+    return el
+}
+
+function createBlur() {
+    const { headerBackground } = getThemeOptions()
+    return $('<div>')
+        .addClass('profile-blur')
+        .css('backgroundImage', `url(${headerBackground})`)
+}
+
+function createAvatar() {
+    return $('<div>')
+        .addClass('profile-avatar')
+        .append(`<a href="${index}"><img src="${avatar}" /></a>`)
+}
+
+function createMessageElements() {
+    return $('<div>')
+        .addClass('profile-msg')
+        .append(
+            $('<p>')
+                .append(
+                    $('<a>')
+                        .addClass('profile-nickname')
+                        .attr('href', index),
+                )
+                .append(
+                    $('<button>')
+                        .addClass('profile-followstate')
+                        .attr('href', index),
+                ),
+        )
+        .append(
+            $('<p>')
+                .append(
+                    $('<span>')
+                        .addClass('profile-age')
+                        .text('园龄：'),
+                )
+                .append(
+                    $('<span>')
+                        .addClass('profile-followers')
+                        .append('<a>')
+                        .attr('href', followersDetailsUrl)
+                        .text('粉丝：'),
+                )
+                .append(
+                    $('<span>')
+                        .addClass('profile-following')
+                        .append('<a>')
+                        .attr('href', followingDetailsUrl)
+                        .text('关注：'),
+                ),
+        )
+}
+
+function insertMessage() {
     const followState = getFollowState()
     const userName = getBlogname()
     const age = getBlogAge()
-    const fans = getFollowers()
-    const focus = getFollowing()
+    const followers = getFollowers()
+    const following = getFollowing()
 
-    const { headerBackground } = getThemeOptions()
-    const el = `
-    <section class="profile">
-        <div class="profile-banner" style="background-image:url(${headerBackground})">
-            <div class="profile-signature"></div>
-        </div>
-        <div class="profile-menu">
-            <a target="_blank" href="${appWz}">收藏</a>
-            <a target="_blank" href="${appIng}">闪存</a>
-            <a target="_blank" href="${appGroup}">小组</a>
-            <a target="_blank" href="${appQ}">博问</a>
-        </div>
-        <div class="profile-blur" style="background-image:url(${headerBackground})"></div>
-        <div class="profile-avatar">
-            <a href="${index}"><img src="${avatar}" /></a>
-        </div>
-        <div class="profile-msg">
-            <p>
-                <a href="${index}"><span>${userName}</span></a>
-                <button class="followState${isOwner() ? ' disabled' : ''}">${
-        followState ? '取消关注' : '关注'
-    }</button>
-            </p>
-            <p>
-                <span>园龄：${age}</span>
-                <span><a href="${followersDetailsUrl}">粉丝：${fans}</a></span>
-                <span><a href="${followingDetailsUrl}">关注：${focus}</a></span>
-            </p>
-        </div>
-    </section>`
-    $('#mainContent').prepend(el)
+    $('.profile-nickname').text(userName)
+    $('.profile-age').append(age)
+    $('.profile-followers').append(followers)
+    $('.profile-following').append(following)
+    $('.profile-followstate').text(followState ? '取消关注' : '关注')
 }
 
 function followAndUnfollow() {
     $('.profile-msg button').click(function() {
         if (isOwner()) {
-            return false
+            return
         }
-        const isUnfollowed = $('.followState').text() === '关注'
+        const isUnfollowed = $('.profile-followstate').text() === '关注'
         if (isUnfollowed) {
             toast('关注成功')
-            $('.followState').text('取消关注')
+            $('.profile-followstate').text('取消关注')
             follow()
         } else {
             toast('取消关注')
-            $('.followState').text('关注')
+            $('.profile-followstate').text('关注')
             unfollow()
         }
     })
 }
 
-function buildProfile() {
-    createElements()
-    followAndUnfollow()
-}
-
 export default () => {
-    poll($('#profile_block a:nth-of-type(2)').length, buildProfile)
+    const container = createContainer()
+    const background = createBackground()
+    const menu = createMenu()
+    const blur = createBlur()
+    const avatar = createAvatar()
+    const messageWrap = createMessageElements()
+
+    container
+        .append(background)
+        .append(menu)
+        .append(blur)
+        .append(avatar)
+        .append(messageWrap)
+
+    $('#mainContent').prepend(container)
+
+    followAndUnfollow()
+    poll($('#profile_block>a').length, insertMessage)
 }
