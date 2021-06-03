93841ddd9214a729357348d84992ac8536b4cf6a
guangzan <923665892@qq.com>
2021-06-03 15:37:05 +0800

    auto deploy

--- awescnb.config.js
+++ awescnb.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    themeName: 'reacg',
+    themeName: 'geek',
     eslint: true,
     openBrowser: true,
     openAnalyzer: false,
@@ -9,7 +9,7 @@ module.exports = {
 // OPTIONS
 // themeName     - 您创建的皮肤文件夹名称（开发环境 | 生产环境）
 // eslint        - 是否开启 eslint （开发环境 | 生产环境）
-// openBrowser   - 是否自动打开浏览器（开发环境）
+// openBrowser   - 是否自动打开浏览器（s开发环境）
 // openAnalyzer  - 是否开启 size 分析（生产环境）
 // cssExtract    - 是否单独抽离 css（生产环境）
 
