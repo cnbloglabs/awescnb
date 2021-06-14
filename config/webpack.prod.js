5217eb4cb2fe413109788bd8e1dc0e2b1eae0e5a
guangzan <923665892@qq.com>
2021-06-14 15:52:15 +0800

    auto deploy

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ const { themeName, openAnalyzer, cssExtract } = require('../awescnb.config')
 const entry = {
     // index: './src/awescnb/load.js',
     // [themeName]: `./src/themes/${themeName}/index.js`,
-    // demo: './src/themes/demo/index.js',
+    demo: './src/themes/demo/index.js',
     /* 同时构建所有皮肤 ↓ */
     bilibili: './src/themes/bilibili/index.js',
     csdn: './src/themes/csdn/index.js',
