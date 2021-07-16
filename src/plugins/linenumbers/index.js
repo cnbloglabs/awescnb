12633a8b7e8d01e85d35ab4b92f9f106d1f13864
guangzan <guangzan1999@outlook.com>
2021-07-16 21:23:50 +0800

    auto deploy

--- src/plugins/linenumbers/index.js
+++ src/plugins/linenumbers/index.js
@@ -15,36 +15,37 @@ import { linenumbersConfig } from 'options/plugins'
 //     })
 // }
 
-function getRow(container) {
-    function pxToNumber(px) {
-        let num = Number(px.replace('px', ''))
-        return num
-    }
+//TODO
+// function getRow(container) {
+//     function pxToNumber(px) {
+//         let num = Number(px.replace('px', ''))
+//         return num
+//     }
 
-    let clone = container.clone().appendTo('body')
+//     let clone = container.clone().appendTo('body')
 
-    // clear some style
-    clone.css({
-        height: 'auto',
-        'padding-top': 0,
-        'padding-bottom': 0,
-        'margin-top': 0,
-        'margin-bottom': 0,
-        visibility: 'hidden',
-    })
+//     // clear some style
+//     clone.css({
+//         height: 'auto',
+//         'padding-top': 0,
+//         'padding-bottom': 0,
+//         'margin-top': 0,
+//         'margin-bottom': 0,
+//         visibility: 'hidden',
+//     })
 
-    // get line-height
-    let style = window.getComputedStyle(clone[0], null)
-    let fontSize = style.fontSize
-    let lineHeight = style.lineHeight === 'normal' ? fontSize : style.lineHeight
+//     // get line-height
+//     let style = window.getComputedStyle(clone[0], null)
+//     let fontSize = style.fontSize
+//     let lineHeight = style.lineHeight === 'normal' ? fontSize : style.lineHeight
 
-    //get row count
-    let height = style.height
-    let row = pxToNumber(height) / pxToNumber(lineHeight)
+//     //get row count
+//     let height = style.height
+//     let row = pxToNumber(height) / pxToNumber(lineHeight)
 
-    clone.remove()
-    return row
-}
+//     clone.remove()
+//     return row
+// }
 
 /**
  * 构建代码行号
