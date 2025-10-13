707af6561d39525f5be57a2d9b35596e3dfd7511
guangzan <guangzan1999@outlook.com>
2025-10-13 09:36:11 +0800

    refactor(组件结构): 重构主页面组件结构，提取Post组件
    将PostDetails和PostComments提取到新的Post组件中
    简化PostDetails组件，移除重复的布局代码
    在Main组件中使用新的Home和Post组件

--- themes/scribe/src/app/components/main/index.tsx
+++ themes/scribe/src/app/components/main/index.tsx
@@ -1,12 +1,12 @@
+import { Home } from 'lucide-preact'
 import { Pagination } from '../pagination'
-import { PostDetails } from '../post-details'
-import { PostList } from '../post-list'
+import { Post } from '../post'
 
 export function Main() {
   return (
     <main className='flex flex-col px-2'>
-      <PostDetails></PostDetails>
-      <PostList></PostList>
+      <Post></Post>
+      <Home></Home>
       <Pagination></Pagination>
     </main>
   )
--- themes/scribe/src/app/components/post-details/index.tsx
+++ themes/scribe/src/app/components/post-details/index.tsx
@@ -7,15 +7,5 @@ export function PostDetails() {
   if (!data) {
     return null
   }
-  return (
-    <div className='h-full w-full'>
-      <div className='flex items-stretch text-[1.05rem] sm:text-[15px] xl:w-full'>
-        <div className='flex min-w-0 flex-1 flex-col'>
-          <div className='mx-auto flex w-full min-w-0 max-w-2xl flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300'>
-            <Markdown html={data}></Markdown>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return <Markdown html={data}></Markdown>
 }
