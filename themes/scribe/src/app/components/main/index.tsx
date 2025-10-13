6d9c871a3f16a884a487d63fe9125e244fce6a6f
guangzan <wang.guangzan2@iwhalecloud.com>
2025-10-13 11:05:11 +0800

    feat(组件): 重构主页和主布局组件结构
    重构主页组件，添加分页组件并调整布局样式
    重构主布局组件，根据当前页面动态渲染不同组件

--- themes/scribe/src/app/components/main/index.tsx
+++ themes/scribe/src/app/components/main/index.tsx
@@ -1,13 +1,20 @@
-import { Home } from 'lucide-preact'
-import { Pagination } from '../pagination'
+import { getCurrentPage } from '@acnb/utils'
+import { Home } from '../home'
 import { Post } from '../post'
 
 export function Main() {
-  return (
-    <main className='flex flex-col px-2'>
-      <Post></Post>
-      <Home></Home>
-      <Pagination></Pagination>
-    </main>
-  )
+  const currentPage = getCurrentPage()
+
+  const renderComponent = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />
+      case 'post':
+        return <Post />
+      default:
+        return null
+    }
+  }
+
+  return <main className='flex flex-col px-2'>{renderComponent()}</main>
 }
