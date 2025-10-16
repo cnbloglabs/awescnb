ed6c766d43772098b22d2134909228bca0bae4f1
guangzan <guangzan1999@outlook.com>
2025-10-16 15:08:48 +0800

    chore(docs): remove deprecated documentation files and components
    - Deleted various documentation files including index.md, usage guides, and theme-related markdown files to streamline the documentation structure.
    - Removed unused TypeScript declaration files and Vue components to clean up the codebase.
    - Eliminated unnecessary assets and configuration files to enhance maintainability and reduce clutter.

--- docs/.vitepress/components/Badge.vue
+++
@@ -1,9 +0,0 @@
-<script lang='ts' setup>
-defineProps<{
-  text: string
-}>()
-</script>
-
-<template>
-  <span class="bg-green-500 rounded px-2 text-sm align-top font-normal text-white">{{ text }}</span>
-</template>
