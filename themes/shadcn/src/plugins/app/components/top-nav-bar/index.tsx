416c03b73ca049dd700822fed94c7281fee53202
guangzan <wang.guangzan2@iwhalecloud.com>
2025-11-13 19:22:18 +0800

    feat(utils): enhance utility functions for user profile and blog navigation
    - Added new utility functions to generate URLs for user details, followers, followees, and blog home.
    - Updated existing functions to improve clarity and consistency in URL generation.
    - Refactored the TopNavBar component to utilize the new getIndexUrl function for navigation.
    - Improved spelling in comments and code for better readability.

--- themes/shadcn/src/plugins/app/components/top-nav-bar/index.tsx
+++ themes/shadcn/src/plugins/app/components/top-nav-bar/index.tsx
@@ -1,4 +1,4 @@
-// import { Github } from 'lucide-preact'
+import { getIndexUrl } from '@acnb/utils'
 import { NavItems } from './nav-items'
 import Signature from './signature.svg?react'
 
@@ -7,23 +7,12 @@ export function TopNavBar() {
     <header className='bg-background'>
       <div className='px-2 sm:px-6 lg:px-6'>
         <div className='flex h-14 items-center justify-end'>
-          <div className='mr-auto flex items-center'>
+          <a href={getIndexUrl()} className='mr-auto flex items-center'>
             <Signature className='h-14 w-14 text-foreground'></Signature>
-          </div>
+          </a>
           <nav className='flex items-center gap-4'>
             <NavItems />
           </nav>
-          {/* <div className='ml-1 flex items-center'>
-            <a
-              href={'https://github.com/cnbloglabs/awescnb'}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='rounded-lg p-2 text-muted-foreground transition-all duration-200 hover:bg-primary/10 hover:text-foreground'
-              aria-label='GitHub'
-            >
-              <Github className='h-5 w-5' />
-            </a>
-          </div> */}
         </div>
       </div>
     </header>
